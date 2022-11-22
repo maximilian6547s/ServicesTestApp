a114d1c21b64a14a9b0a582ce19bc3ddd208fb58
Maxim Sakharov <sakharov.ma@sberleasing.ru>
2022-11-21 19:22:35 -0600

    foreground services example rework

--- app/src/main/java/com/example/servicestestapp/MainActivity.kt
+++ app/src/main/java/com/example/servicestestapp/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.servicestestapp
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.app.NotificationCompat
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.servicestestapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var id = 0
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -22,36 +17,9 @@ class MainActivity : AppCompatActivity() {
         binding.simpleService.setOnClickListener {
             startService(MyService.newIntent(this, 25))
         }
-        binding.foregroundService.setOnClickListener{
-            showNotification()
+        binding.foregroundService.setOnClickListener {
+            ContextCompat.startForegroundService(this, MyForegroundService.newIntent(this))
         }
     }
 
-    private fun showNotification() {
-
-        //From API 26 we need to use notification
-        // otherwise service will not restart after app will be killed
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(
-                CHANNEL_ID,
-                CHANNEL_NAME,
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            notificationManager.createNotificationChannel(notificationChannel)
-        }
-
-        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setContentTitle("Title")
-            .setContentText("Text")
-            .setSmallIcon(R.drawable.ic_launcher_background)
-            .build()
-
-        notificationManager.notify(id++, notification)
-    }
-
-    companion object{
-        private const val CHANNEL_ID = "channel_id"
-        private const val CHANNEL_NAME = "channel_name"
-    }
 }
---
+++ app/src/main/java/com/example/servicestestapp/MyForegroundService.kt
@@ -0,0 +1,84 @@
+package com.example.servicestestapp
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.*
+
+
+class MyForegroundService : Service() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+        createNotificationChannel()
+        startForeground(NOTIFICATION_ID, createNotification())
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        log("onStartCommand")
+        coroutineScope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer $i")
+            }
+        }
+
+        //this flag restarts service if system kills him
+        return START_STICKY
+        //this flag restarts service if system kills him and will redeliver intent if we pass them
+//        return START_REDELIVER_INTENT
+//this flag DOES NOT restarts service if system kills him
+//        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        log("onDestroy")
+        coroutineScope.cancel()
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    private fun log(message: String) {
+        Log.d("SERVICE_TAG", "MyForegroundService: $message")
+    }
+
+    private fun createNotificationChannel() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private fun createNotification() = NotificationCompat.Builder(this, CHANNEL_ID)
+        .setContentTitle("Title")
+        .setContentText("Text")
+        .setSmallIcon(R.drawable.ic_launcher_background)
+        .build()
+
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
+        private const val NOTIFICATION_ID = 1
+
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MyForegroundService::class.java)
+        }
+    }
+}
