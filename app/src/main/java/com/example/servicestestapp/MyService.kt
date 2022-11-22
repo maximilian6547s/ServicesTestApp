2def62ef4a9f4d0737169134cb63a20cdd05564e
Maxim Sakharov <sakharov.ma@sberleasing.ru>
2022-11-21 18:31:29 -0600

    simple service example

---
+++ app/src/main/java/com/example/servicestestapp/MyService.kt
@@ -0,0 +1,51 @@
+package com.example.servicestestapp
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.*
+
+
+class MyService : Service() {
+
+    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+        log("onCreate")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        log("onStartCommand")
+        coroutineScope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+                log("Timer $i")
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        log("onDestroy")
+        coroutineScope.cancel()
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    private fun log(message:String) {
+        Log.d("SERVICE_TAG", "MyService: $message")
+    }
+
+    companion object {
+
+        fun newIntent(context: Context):Intent {
+            return Intent(context, MyService::class.java)
+        }
+    }
+}
